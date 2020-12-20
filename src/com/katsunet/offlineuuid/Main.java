e58e4dcef20de106c6cc180f95512f70e6f1a0d2
Joaquim Sardiña <joaquimsb89@gmail.com>
2020-12-20 20:06:02 +0100

    Code format

--- src/com/katsunet/offlineuuid/Main.java
+++ src/com/katsunet/offlineuuid/Main.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public class Main {
 	public static void main(String[] args) {
-		if(args.length==1){
+		if (args.length == 1) {
 			System.out.print(UUID.nameUUIDFromBytes(("OfflinePlayer:" + args[0]).getBytes()));
 		}
 	}
